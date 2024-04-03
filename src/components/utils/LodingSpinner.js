281e854712a2ab14652718f7e10a2778749ea858
harshleet <harshguptalnm@gmail.com>
2024-04-03 18:54:48 +0530

    Update LodingSpinner.js

--- src/components/utils/LodingSpinner.js
+++ src/components/utils/LodingSpinner.js
@@ -1,41 +1,12 @@
-import React, { useEffect, useState } from "react";
-import Lottie from "react-lottie";
-import loadingAnimation from "./loadingAnimation.json";
+import React from "react";
 
-const LoadingSpinner = () => {
-  const [showSpinner, setShowSpinner] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSpinner(true);
-    }, 1000); // Set the delay duration here (1000 milliseconds = 1 second)
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: loadingAnimation,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
+import { Spinner } from "flowbite-react";
+const LodingSpinner = () => {
   return (
-    showSpinner && (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <Lottie options={defaultOptions} height={120} width={120} />
-      </div>
-    )
+    <div>
+      <Spinner aria-label="Extra large spinner example" size="xl" className=" h-screen"/>
+    </div>
   );
 };
 
-export default LoadingSpinner;
+export default LodingSpinner;
